0f33379723cb648758010db1af7c588a80410a67
Santosh Erra <santosh.erra@gmail.com>
2022-10-06 00:39:07 +0530

    added eureka client config in order service

--- order-service/src/main/java/com/dailycodebuffer/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/dailycodebuffer/orderservice/OrderServiceApplication.java
@@ -2,9 +2,10 @@ package com.dailycodebuffer.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
